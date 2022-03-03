d0ed4e612f36fe3bb278f6a823831ea7f1a22b5b
ali <anehrani@ekin.com>
2022-03-04 00:08:06 +0300

    adding tutorial file

---
+++ mesh3dprojects/cmake-build-debug/CMakeFiles/mesh3dprojects.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mesh3dprojects.dir/main.cpp.o"
+  "mesh3dprojects"
+  "mesh3dprojects.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mesh3dprojects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
